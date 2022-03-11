cde12549de9fd861f70660b33438f8d4acfb7c6a
lishuailibertine <lishuai19@yeah.net>
2022-03-11 14:20:37 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .package(name:"Blake2",url: "https://github.com/tesseract-one/Blake2.swift.git", from: "0.1.2"),
         .package(name:"swift-scale-codec",url: "https://github.com/tesseract-one/swift-scale-codec.git", from: "0.2.0"),
         .package(name:"BIP39swift", url: "https://github.com/mathwallet/BIP39swift", from: "1.0.0"),
-        .package(name:"Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift", from: "1.2.0")
+        .package(name:"Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift", from: "1.2.6")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
